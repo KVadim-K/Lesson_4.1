726c8ea67509595a000af0f41b92b39973b2278a
KVadim-K <kvadimk@yandex.ru>
2024-05-10 20:43:25 +0300

    end warrior & add bank

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ class warrior():
         self.power += 5
     def eat(self):
         print(f"{self.name} сел поесть")
+        self.endurance += 3
+        self.power += 10
+
     def hit(self):
         print(f"{self.name} нанес удар")
         self.endurance -= 6
+        self.power -= 8
     def wolk(self):
         print(f"{self.name} идет")
     def info(self):
@@ -20,6 +24,21 @@ class warrior():
         print(f"Сила война: {self.power}")
         print(f"Выносливость война: {self.endurance}")
         print(f"Цвет волос война: {self.hair_color}")
+war1 = warrior( "Вадим", 100, 100, hair_color = "блондин")
+war2 = warrior( "Антон", 80, 75, hair_color = "шатен")
+
+print(war1.name, war1.power, war1.endurance, war1.hair_color)
+war1.sleep()
+war1.eat()
+war1.hit()
+war1.wolk()
+print(war1.name, war1.power, war1.endurance, war1.hair_color)
+print(war2.name, war2.power, war2.endurance, war2.hair_color)
+war2.sleep()
+war2.eat()
+war2.hit()
+war2.wolk()
+print(war2.name, war2.power, war2.endurance, war2.hair_color)
 
 
 
